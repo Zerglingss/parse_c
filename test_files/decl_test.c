38907acd4f874f738892ca0903309db66242cfdb
Zerglingss <131566242+Zerglingss@users.noreply.github.com>
2025-01-24 17:08:17 -0500

    Very basic tests
    Added some test files that run.

---
+++ test_files/decl_test.c
@@ -0,0 +1,26 @@
+
+
+void foo();
+int bar(int a, int b, int c);
+
+int global_int;
+char global_char;
+
+
+struct new_struct {
+    int a;
+    int b;
+};
+
+int main()
+{
+    global_int;
+    global_char;
+
+
+    int a = 7;
+    int b = 12;
+    
+    foo();
+    return a + b;
+}
---
+++ test_files/sample2.c
@@ -0,0 +1,9 @@
+
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  return 0;
+}
